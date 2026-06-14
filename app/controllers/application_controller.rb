ca1c9ff46110fe7a094069a8d5dcdc77687ae2d8
emilesu <emilesu@qq.com>
2026-06-14 16:44:18 +0800

    新增功能：添加Pundit权限控制基础框架与测试文件

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    # app/controllers/application_controller.rb
+    include Pundit
+    rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+    private
+    def user_not_authorized
+        flash[:alert] = "您暂无权限访问该页面"
+        redirect_to request.referer || root_path
+    end
 end
