456d5479de882aae830adab9b1858c82c64ef951
emilesu <emilesu@qq.com>
2026-06-30 10:17:32 +0800

    修复：微信/Google OAuth回调返回404 - 移除Provider重复注册
    config/initializers/omniauth.rb中通过OmniAuth::Builder重复注册
    了wechat和google_oauth2 Provider，与devise.rb中config.omniauth
    重复，导致中间件栈中出现两个相同策略实例，回调处理冲突返回404。

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,19 +3,3 @@ OmniAuth.config.allowed_request_methods = [:post, :get]
 OmniAuth.config.on_failure = proc do |env|
   SessionsController.action(:oauth_failure).call(env)
 end
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :wechat,
-           ENV['WECHAT_APP_ID'],
-           ENV['WECHAT_APP_SECRET'],
-           scope: 'snsapi_login',
-           request_path: '/users/auth/wechat',
-           callback_path: '/users/auth/wechat/callback'
-
-  provider :google_oauth2,
-           ENV['GOOGLE_CLIENT_ID'],
-           ENV['GOOGLE_CLIENT_SECRET'],
-           scope: 'email,profile', access_type: 'online',
-           request_path: '/users/auth/google_oauth2',
-           callback_path: '/users/auth/google_oauth2/callback'
-end
