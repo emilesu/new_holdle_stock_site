bdd58591271ed785bc7915aa2ce1d6f136ec049b
emilesu <emilesu@qq.com>
2026-06-30 11:54:25 +0800

    fix: 还原 allowed_request_methods 为仅 POST，添加失败日志
    - OmniAuth 的 allowed_request_methods 移除 :get，恢复为默认 [:post]
      与 omniauth-rails_csrf_protection 保持设计一致
    - on_failure 处理器增加日志记录方便定位问题

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,10 @@
 OmniAuth.config.logger = Rails.logger
-OmniAuth.config.allowed_request_methods = [:post, :get]
+OmniAuth.config.allowed_request_methods = [:post]
 OmniAuth.config.on_failure = proc do |env|
+  error_type = env['omniauth.error.type']
+  error = env['omniauth.error']
+  strategy = env['omniauth.error.strategy']
+  Rails.logger.error "[OmniAuth Failure] type=#{error_type} strategy=#{strategy&.name} error=#{error&.message}"
   Rack::Response.new(
     ['<html><body><script>window.location.href="/users/sign_in";</script></body></html>'],
     302,
